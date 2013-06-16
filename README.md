b9725308fc9f87bcc75a1d9444d4dfe12fefd8e7
Yuuki Tan-nai <yuuki.tannai@gaiax.com>
2013-06-16 17:01:22 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Acme::EvaTitlenize - It's new $module
+
+# SYNOPSIS
+
+    use Acme::EvaTitlenize;
+
+# DESCRIPTION
+
+Acme::EvaTitlenize is ...
+
+# LICENSE
+
+Copyright (C) Yuuki Tan-nai.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Yuuki Tan-nai <yuuki.tannai@gaiax.com>
---
+++ lib/Acme/EvaTitlenize.pm
@@ -0,0 +1,39 @@
+package Acme::EvaTitlenize;
+use 5.008005;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Acme::EvaTitlenize - It's new $module
+
+=head1 SYNOPSIS
+
+    use Acme::EvaTitlenize;
+
+=head1 DESCRIPTION
+
+Acme::EvaTitlenize is ...
+
+=head1 LICENSE
+
+Copyright (C) Yuuki Tan-nai.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Yuuki Tan-nai E<lt>yuuki.tannai@gaiax.comE<gt>
+
+=cut
+
