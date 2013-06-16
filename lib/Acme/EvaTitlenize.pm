d125de3dc4f52d515a435f8ae0951ee197ef00b0
papix <mail@papix.net>
2013-06-16 20:45:17 +0900

    必要な機能を lib/Acme/EvaTitlenize.pm に実装した.

--- lib/Acme/EvaTitlenize.pm
+++ lib/Acme/EvaTitlenize.pm
@@ -2,12 +2,36 @@ package Acme::EvaTitlenize;
 use 5.008005;
 use strict;
 use warnings;
+use utf8;
+
+use Text::VisualWidth::UTF8;
 
 our $VERSION = "0.01";
 
+sub lower_left {
+    my ($left, $low) = @_;
 
+    my $evanize;
+
+    $evanize .= join "\n", split //, $left;
+    $evanize .= $low;
+
+    return $evanize;
+}
+
+sub upper_right {
+    my ($up, $right) = @_;
+
+    my $space = ' ' x Text::VisualWidth::UTF8::width($up);
+    my $evanize = $up;
+
+    $evanize .= join "\n$space", split //, $right;
+
+    return $evanize;
+}
 
 1;
+
 __END__
 
 =encoding utf-8
